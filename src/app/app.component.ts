93ba2da44021ed1f1470d57742bdbe4ad4c73344
MariamAntsupova <mariam.antsupova.1@btu.edu.ge>
2024-12-19 15:45:00 +0400

    change texts by clicking items with ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'switchTask';
+  selectedItem: string = 'Teapot';
+
+  items = ['Teapot', 'Lamp', 'Phone', 'Television', 'Fishbowl'];
+
 }
